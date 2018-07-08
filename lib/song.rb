297908a618fbaa9e2f5960e8c040d88c7daea2b7
Emily Wessel <ettl02@gmail.com>
2018-07-08 13:55:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,22 +2,23 @@ require 'pry'
 
 class Song
 
+  attr_accessor :name
+  attr_reader :artist
+
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
 
-  attr_accessor :name
-  attr_reader :artist
-
   @@songs = []
+
+  def self.all
+    @@songs
+  end
 
   def artist=(artist)
     @artist = artist
     artist.add_song(self) unless artist.songs.include?(self)
   end
 
-  def self.all
-    @@songs
-  end
 end
