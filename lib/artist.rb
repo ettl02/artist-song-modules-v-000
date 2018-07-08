ff1e2142ea0730e14f56a8ed2f9ca4885a1cef25
Emily Wessel <ettl02@gmail.com>
2018-07-08 13:46:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,10 @@ require 'pry'
 class Artist
 
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
 
 
   attr_accessor :name
@@ -34,7 +37,7 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
