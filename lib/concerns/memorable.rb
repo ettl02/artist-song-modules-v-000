c8bec702fb33319d986d1c36ac7b57c71c9d06db
Emily Wessel <ettl02@gmail.com>
2018-07-08 13:52:14 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,18 @@
 module Memorable
   module ClassMethods
 
-def reset_all
-  self.all.clear
-end
-
-def count
-  self.all.count
-end
-
-module InstanceMethods
-  def initialize
-    self.class.all << self
+  def reset_all
+    self.all.clear
   end
+
+  def count
+    self.all.count
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
 
 end
 end
