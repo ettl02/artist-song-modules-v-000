01ba9eb6fbb1ebf659c7e4bb7a7e5d30039c4d4b
Emily Wessel <ettl02@gmail.com>
2018-07-08 13:22:20 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,13 +2,9 @@ module Findable
   module find_by_name
 
     def find_by_name(name)
-      @@artists.detect{|a| a.name == name}
-    end
+       self.all.detect{|o| o.name}
 
 
-    def find_by_name(name)
-      @@songs.detect{|a| a.name == name}
-    end
 
   end
 end
